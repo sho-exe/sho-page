783ebfaf026264958cd7160684b07e7d50e28a70
sho <109146811+sho-exe@users.noreply.github.com>
2025-12-17 10:46:44 +0800

    Refactor student data handling and UI updates

--- script.js
+++ script.js
@@ -3,52 +3,73 @@
 // 1. Define Multi-Dimensional Array studentData)
 // Format: [Name (String), Credit Hour (Number), Current GPA (Number)]
 
+let test = document.getElementById("test")
+
+let body = document.getElementById("body")
+
+body.innerHTML += '<div>TESTT</div>';
+
+
+test.innerHTML = "tesasdsst";
+
 let studentData = [
     ["Ahmad", 3, 3.5],
     ["Abu", 4, 3.6],
     ["Ali", 2, 3.2],
 ]
 
+function addData() {
+    let studName = document.getElementById("studName").value;
+    let creditHours = document.getElementById("creditHours").value;
+    let currentGPA = document.getElementById("currentGpa").value;
+
+    studentData.push([studName, creditHours, currentGPA]);
+    // alert(studName);
+    // alert(creditHours);
+    // alert(currentGPA);
+    measureDean();
+    refreshList();
+
+    
+}
+
+function refreshList() {
+    let result = document.getElementById("result");
+    result.innerHTML = "";
+
+    for (let i = 0; i < studentData.length; i++) {
+        result.innerHTML += `
+            <div>
+                <b>Name: ${studentData[i][0]}</b><br>
+                <b>Current GPA: ${studentData[i][2]}</b><br>
+                <b class="${studentData[i][3] === 'DEAN LIST' ? 'eligible' : 'not-eligible'}">
+                    Status: ${studentData[i][3]}
+                </b>
+                <hr>
+            </div>
+        `;
+    }
+}
+
+
+
 
 
 // 2. Create function to measure dean list eligibility using conditional statement
 
+function measureDean() {
+    for (let i = 0; i < studentData.length; i++) {
+        if (studentData[i][2] >= 3.5) {
+            studentData[i].push("DEAN LIST");
+        } else {
+            studentData[i].push("NOT DEAN LIST");
 
-for (let i = 0; i < studentData.length; i++) {
-    if (studentData[i][2] >= 3.5) {
-        studentData[i].push("DEAN LIST");
-    } else {
-        studentData[i].push("NOT DEAN LIST");
-
+        }
     }
-}
-
-document.write("<div class='output-box'>");
-document.write("<h2>Section 03 Result</h2>");
-
-// 3.Create Looping (for loop) to print student data such name, credithours and current gpa and their eligibility (status)
-
-
-// 4. print all the output, this one need to be in the loop
-
-for (let i = 0; i < studentData.length; i++) {
-    document.write("<div>");
-    document.write("<b>Name : " + studentData[i][0] + "</b><br>");
-    document.write("<b>Current GPA:" + studentData[i][2] + "</b><br>");
-
-
-    if (studentData[i][3] == "DEAN LIST" ) {
-        document.write("<b style='color:green'>Status: " + studentData[i][3] + "</b><br>");
-
-    } else {
-        document.write("<b style='color:red'>Status: " + studentData[i][3] + "</b><br>");
-
-    }
-
-    document.write("<hr style='border-top: 1px dotted #ccc;'>");
-    document.write("</div>");
 
 }
 
-//here need to put closed-loop bracket
-document.write("</div>");
+
+
+
+
